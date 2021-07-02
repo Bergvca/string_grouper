166fe2e69aa5d20c2740aad1b15aa799afcc5d0c
bergvca <cvandenberg>
2021-07-02 15:18:16 +0200

    Updated version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [0.4.1?] - 2021-06-11
+## [0.5.0] - 2021-06-11
 
 ### Added
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='string_grouper',
-    version='0.4.0',
+    version='0.5.0',
     packages=['string_grouper'],
     license='MIT License',
     description='String grouper contains functions to do string matching using TF-IDF and the cossine similarity. '
