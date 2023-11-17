a78dcac65ea1b4c7dea10e83fdd88fd21db2f3f4
Robin Knox <robin.knox@clearscore.com>
2023-11-17 10:27:56 +0000

    Ensure requirements are more explicit to create reproducible builds

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ setup(
     url='https://github.com/Bergvca/string_grouper',
     zip_safe=False,
     python_requires='>3.7',
-    install_requires=['pandas>=0.25.3'
-                      , 'scipy'
-                      , 'scikit-learn'
-                      , 'numpy'
+    install_requires=['pandas>=2.1.0'
+                      , 'scipy>=1.11.2'
+                      , 'scikit-learn>=1.3.0'
+                      , 'numpy>=1.16.6'
                       , 'sparse_dot_topn_for_blocks>=0.3.1'
                       , 'topn>=0.0.7'
                       ]
