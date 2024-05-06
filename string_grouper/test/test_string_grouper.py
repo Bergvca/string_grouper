2f95b77fdd9aefd3a090495be97dac1176ef8567
Ruben Menke <rum@bankingcircle.com>
2024-05-06 19:43:39 +0200

    fix corresponding test

--- string_grouper/test/test_string_grouper.py
+++ string_grouper/test/test_string_grouper.py
@@ -480,7 +480,7 @@ class StringGrouperTest(unittest.TestCase):
 
         expected_matches = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]])
         np.testing.assert_array_equal(
-            expected_matches, sg._build_matches(master, dupe)[0].toarray()
+            expected_matches, sg._build_matches(master, dupe).toarray()
         )
 
     def test_build_matches_list(self):
